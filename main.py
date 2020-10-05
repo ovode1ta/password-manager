691cdcfa99e6b44b5a716e966870a56d1880db22
cgdisgood <71853433+cgdisgood@users.noreply.github.com>
2020-10-05 17:53:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import random
 import string
-
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-+=[{]}|<,>.'
 password = ''
 for c in range(20):
     password += random.choice(chars)
 print("Here is your password:\n" + password)
-print('This password takes on average ~300 quintillion years to crack, according to howsecureismypassword.net')
-end = input("Press enter to close, reopen for a new password.")
+service = input("What are you using this password for? \n")
+dl = input("Would you like to download this password? y/n\n")
+
+
+if dl == "y":
+    f = open("passwords.txt", "a")
+    f.write(password + " <-- Password for " + service + "\n")
+    f.close()
+    end = input("Your password has been saved. Press ENTER to finish.")
+if dl == "n":
+    input("Press enter to close the program")
 
